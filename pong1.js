2e6cd323dbd137349cef1e037c0deb04801e6be3
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-09-11 16:34:44 -0400

    fixed lag due to redrawing of object

--- pong1.js
+++ pong1.js
@@ -2,10 +2,10 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 // player paddle position and dimensions
-var playerX = 20;
-var playerY = 40;
-var playerHeight = 100;
-var playerWidth = 20;
+let playerX = 20;
+let playerY = 40;
+const playerHeight = 100;
+const playerWidth = 20;
 
 // computer paddle position and dimensions
 var computerX = 660;
@@ -22,14 +22,24 @@ var ballHeight = 20;
 var ballVelocityX = 5;
 var ballVelocityY = 5;
 
-
 // scores
 var playerScore = 0;
 var computerScore = 0;
 var computerSpeed = 4.5;
-var deltaY;
 
 var update = () => {
+  // draw canvas,player, computer and ball to screen
+  context.fillStyle = 'black';
+  context.strokeStyle = 'white';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = 'white';
+  context.font = '50px Arial';
+  context.fillText(`${playerScore}`, 200, 80);
+  context.fillText(`${computerScore}`, 450, 80);
+  context.fillRect(playerX, playerY, playerWidth, playerHeight);
+  context.fillRect(computerX, computerY, computerWidth, computerHeight);
+  context.fillRect(ballX, ballY, ballWidth, ballHeight);
+  context.fill();
   context.save();
 
   // reset ball to center
@@ -37,7 +47,7 @@ var update = () => {
     ballX = canvas.width / 2;
     ballY = canvas.height / 2;
     ballVelocityX = -ballVelocityX || ballVelocityX;
-  }
+  };
 
   // make ball move
   ballX += ballVelocityX;
@@ -50,8 +60,9 @@ var update = () => {
 
   // bounce ball and score point for computer if ball misses paddle
   if (ballX < 0 + playerX) {
+    debugger;
+    // ball is over or under player paddle
     if (ballY > playerY && ballY < playerHeight + playerY) {
-      deltaY = ballY - (computerY + computerHeight / 2);
       ballVelocityX = -ballVelocityX;
     } else {
       computerScore++;
@@ -62,7 +73,6 @@ var update = () => {
   if (ballX > canvas.width - 40) {
     if (ballY > computerY && ballY < computerHeight + computerY) {
       ballVelocityX = -ballVelocityX;
-      deltaY = ballY - (computerY + computerHeight / 2);
     } else {
       playerScore++;
       reset();
@@ -76,31 +86,13 @@ var update = () => {
   }
 
   // on mouse move event, change position of playerY to position of mouse
-  let movePlayerPaddle = (event) => {
+  let movePlayerPaddle = event => {
     playerY = event.clientY - playerHeight;
-  }
+  };
   canvas.addEventListener('mousemove', movePlayerPaddle);
-
-
-  // draw canvas,player, computer and ball to screen
-  context.fillStyle = 'black';
-  context.strokeStyle = 'white'
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = 'white';
-  context.font = '50px Arial'
-  context.fillText(`${playerScore}`, 200, 80);
-  context.fillText(`${computerScore}`, 450, 80);
-  context.fillRect(playerX, playerY, playerWidth, playerHeight);
-  context.fillRect(computerX, computerY, computerWidth, computerHeight);
-  context.fillRect(ballX, ballY, ballWidth, ballHeight);
-  context.moveTo(canvas.width / 2, 0);
-  context.lineTo(0, canvas.height);
-  context.fill();
-
 
   // run update function every frame
   window.requestAnimationFrame(update);
-  context.restore();
-}
+};
 
-window.onload = update
+window.onload = update;
